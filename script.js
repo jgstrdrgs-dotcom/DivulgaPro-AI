f4eb871b42dbdd7f1649b440d708988c4d9b18f3
jgstrdrgs-dotcom <jgstrdrgs@gmail.com>
2026-09-15 20:14:44 -0300

    refactor(ui): consolida sugestoes rapidas

--- script.js
+++ script.js
@@ -72,11 +72,6 @@ let campaigns = [
 
 const suggestions = [
   "Criar uma campanha",
-  "Quero divulgar um tênis feminino.",
-  "Quero criar uma promoção para minha loja.",
-  "Quero fazer Stories para um produto.",
-  "Quero criar um vídeo para vender este produto.",
-  "Quero montar uma campanha para o WhatsApp.",
   "Criar Stories",
   "Criar roteiro de vídeo",
   "Criar oferta",
